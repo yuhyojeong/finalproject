b58ec65f05aeb242863535ea2eb3fda471a253d3
hyojyu <reve0515@gmail.com>
2023-06-18 03:38:49 +0900

    commit6

---
+++ Screen_manager.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <fstream>
+#include <Windows.h>
+#include <conio.h>
+#include <chrono>
+#include <vector>
+#include "Screen_manager.h"
+#include <thread>
+
+using namespace std;
+
+//move cursor
+void cursorYX(int y, int x)
+{
+    COORD pos;
+    pos.X = x;        
+    pos.Y = y;            
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void Screen_manager::render(){
+    for(int j=0; j<height; j++){
+        for(int i=0; i<width; i++){
+            cursorYX(j, i);
+            printf("%c", board[j][i]);
+        }
+    }
+}
+
+//print sharing things.
+void Screen_manager::print_share(){
+    //wall
+    for(int i=0; i<width; i++){board[height-1][i]='w';}
+    for(int j=0; j<height; j++){board[j][0]='w'; board[j][width-1]='w';}
+
+}
+
+//print when key didn't pressed
+void Screen_manager::print(){
+    board[this->my_plane.y][this->my_plane.x]='M';
+    print_share();
+}
+
+//print when key pressed
+void Screen_manager::print(int ch){ //ascii
+    if(ch=='d'){ //right
+        if(this->my_plane.y >=0 && this->my_plane.y <(height-1) && this->my_plane.x >0 && this->my_plane.x < (width-2)){
+            board[this->my_plane.y][this->my_plane.x]=' ';
+            this->my_plane.x +=1;
+            board[this->my_plane.y][this->my_plane.x]='M';
+        }
+    }
+    else if(ch=='a'){ //left
+        if(this->my_plane.y >=0 && this->my_plane.y <(height-1) && this->my_plane.x >1 && this->my_plane.x < (width-1)){
+            board[this->my_plane.y][this->my_plane.x]=' ';
+            this->my_plane.x -=1;
+            board[this->my_plane.y][this->my_plane.x]='M';
+        }
+    }
+    else if(ch=='w'){ //up
+        if(this->my_plane.y >0 && this->my_plane.y <(height-1) && this->my_plane.x >0 && this->my_plane.x < (width-1)){
+            board[this->my_plane.y][this->my_plane.x]=' ';
+            this->my_plane.y -=1;
+            board[this->my_plane.y][this->my_plane.x]='M';
+        }
+    }
+    else if(ch=='s'){ //down
+        if(this->my_plane.y >=0 && this->my_plane.y <(height-2) && this->my_plane.x >0 && this->my_plane.x < (width-1)){
+            board[this->my_plane.y][this->my_plane.x]=' ';
+            this->my_plane.y +=1;
+            board[this->my_plane.y][this->my_plane.x]='M';
+        }    
+    }
+
+    print_share();
+}
+
+void Screen_manager::extrabullet(int y, int x, bool p){
+    if (p == true){
+        if (x >=2){
+            board[y][x - 1]=' ';
+        }
+        if (x <= 57){
+            board[y][x + 1]=' ';
+        }
+    }
+}
